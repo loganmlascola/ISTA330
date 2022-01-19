b50c621b0c84eaa7ffa58cab7f2c30aab1855f0b
loganmlascola <loganmlascola@gmail.com>
2022-01-19 12:21:23 -0700

    js notes

---
+++ practicum/lecture notes/jan-18.js
@@ -0,0 +1,13 @@
+/*
+1. how to declare/define variables
+2. what are the types
+3. how to define conditionals (syntax of if)
+4. how to repeat doing a thing (syntax of loops)
+*/
+
+// two ways to define variables below
+var x = 1; 
+let y = 2;
+// defined value but undefined below
+var w;
+let z;
